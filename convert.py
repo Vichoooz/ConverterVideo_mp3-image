777de5e29f723f3dc54f38180bff44543d94a272
Vichoooz <vichomr2001@gmail.com>
2025-07-13 20:48:56 -0400

    fix auth

--- convert.py
+++ convert.py
@@ -1,27 +1,6 @@
 import os
 from moviepy.editor import *
 from PIL import Image, ImageOps
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from googleapiclient.http import MediaFileUpload
-
-def autenticar_youtube():
-    """Autentica al usuario con la API de YouTube y devuelve un objeto de servicio."""
-    creds = None
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('client_secret.json', SCOPES)
-            creds = flow.run_local_server(port=8080)
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-    return build('youtube', 'v3', credentials=creds)
 
 def VideoImagenConverter(output_directory): 
     directory_path = "./"
@@ -36,6 +15,8 @@ def VideoImagenConverter(output_directory):
 
     if len(image_files) != len(audio_files):
         print("Las listas de imágenes y audios no tienen la misma longitud.")
+        print(f"Imágenes: {len(image_files)}")
+        print(f"Audios: {len(audio_files)}")
         return False
 
     for i, (image_file, audio_file) in enumerate(zip(image_files, audio_files)):
@@ -71,43 +52,7 @@ def VideoImagenConverter(output_directory):
         except Exception as e:
             print(f"Ocurrió un error al procesar {image_file} y {audio_file}: {e}")
 
-def upload_video(file_path, title, description, tags=None, category_id="22", privacy_status="private"):
-    media = MediaFileUpload(file_path, mimetype="video/mp4", resumable=True)
-    request_body = {
-        "snippet": {
-            "title": title,
-            "description": description,
-            "tags": tags or [],
-            "categoryId": category_id,
-        },
-        "status": {
-            "privacyStatus": privacy_status
-        }
-    }
-
-    request = youtube.videos().insert(part="snippet,status", body=request_body, media_body=media)
-    response = request.execute()
-    print(f"Video subido exitosamente. ID del video: {response['id']}")
-    return f"https://www.youtube.com/watch?v={response['id']}"
-
-
-autor = "Moises Muñoz"
 # Definir ruta de salida y autenticación
 output_directory = "./output_videos/"
-SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
 
-# Crear videos y subirlos
 VideoImagenConverter(output_directory)
-youtube = autenticar_youtube()
-print("Autenticación exitosa")
-
-# Escribir links en archivo
-with open("link.txt", "w") as archivo:
-    for file in os.listdir(output_directory):
-        if file.endswith(".mp4"):
-            file_path = os.path.join(output_directory, file)
-            title = file[:-10] + " - " + autor  # Remueve los últimos 10 caracteres
-            description = "Fotografías de "+autor+" con música de fondo."
-            tags = ["Musica", "Fotografia", "Moises Muñoz"]
-            link = upload_video(file_path, title, description, tags=tags, privacy_status="private")
-            archivo.write(link + "\n")
